43c2a6291b841099e7339d71518eb62c97d91cc8
Bianca Barcelos <biancamariamichels@gmail.com>
2024-04-01 16:09:17 +0100

    Adicionar resultados com grau de IMC

--- src/main/java/com/bianca/CalculadoraImc.java
+++ src/main/java/com/bianca/CalculadoraImc.java
@@ -15,11 +15,26 @@ public class CalculadoraImc {
         return peso / (altura * altura);
     }
 
+    private String verificarGrauImc(double imc) {
+        if (imc < 18.5) {
+            return "0 - Magreza";
+        } else if (imc < 25) {
+            return "0 - Normal";
+        } else if (imc < 30) {
+            return "1 - Sobrepeso";
+        } else if (imc < 40) {
+            return "2 - Obesidade";
+        } else {
+            return "3 - Obesidade Grave";
+        }
+    }
+
     public void mostrarImc() {
         double imc = calcularImc();
+        String grauImc = verificarGrauImc(imc);
         System.out.printf("""
-                O IMC de %s é: %f.
-                %n""", nome, imc);
+                O IMC de %s é: %f.%n
+                Classificado no grau: %s.""", nome, imc, grauImc);
     }
 }
 
