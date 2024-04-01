f781e2db697d7cbe5fcd4c1b19ee1b8aecf30978
Bianca Barcelos <biancamariamichels@gmail.com>
2024-04-01 15:39:42 +0100

    Ler dados do usuário da linha de comando

--- src/main/java/com/bianca/Main.java
+++ src/main/java/com/bianca/Main.java
@@ -1,7 +1,21 @@
 package com.bianca;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Digite o seu nome: ");
+        String nome = s.nextLine();
+
+        System.out.println("Digite a sua altura: ");
+        double altura = s.nextDouble();
+
+        System.out.println("Digite o seu peso: ");
+        double peso = s.nextDouble();
+
+        CalculadoraImc imc = new CalculadoraImc(nome, altura, peso);
+        imc.mostrarImc();
     }
 }
